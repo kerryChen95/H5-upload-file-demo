5c372f90bcb122269909d308d847f7687cdff6ce
kerry95 <95kerry@gmail.com>
2014-02-27 20:29:05 +0800

    Use form-data to upload

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
+
+// app.use(express.json());
+// app.use(express.urlencoded());
+app.use(express.bodyParser({
+    uploadDir: './tmp'
+}));
+
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,7 +26,12 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.post('/file', routeFile);
+app.post('/userfile', function(req, res, next){
+    // console.log(req);
+    // console.log(req.files);
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send('File uploaded!');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
